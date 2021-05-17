c23581178e4867279d4986e7c96fb1cde6961f7e
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-05-17 10:38:22 -0400

    Update Again

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
+const http = require('http').createServer()
 const WebSocket = require('ws')
-
-app.use(express.static('public'))
-
-app.get('/', function (req, res) {
-})
-
-app.use(function (req, res, next) {
-  res.status(404).sendFile(__dirname + '/public/' + '404.html')
-})
 
 const wss = new WebSocket.Server({ server: http })
 
