f8a65ac4efffb349414d3497b5ffe910875a099c
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-05-19 12:30:05 -0400

    Update

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const http = require('http').createServer(app)
 const WebSocket = require('ws')
 
 app.get('/g&p', function (req, res) {
-  res.sendFile(__dirname + "/ground and platforms");
+  res.sendFile(__dirname + '/ground and platforms')
 })
 
 const wss = new WebSocket.Server({ server: http })
 
 let players = [1]
+const playersInGame = {}
 
 wss.on('connection', function connection (ws) {
   const number = players[players.length - 1]
@@ -22,10 +23,13 @@ wss.on('connection', function connection (ws) {
   players.push((number + 1))
 
   ws.on('message', (message) => {
-    if (!message.startsWith('Player') || !message == "heartbeat") {
+    if (!message.startsWith('Player') || !message === 'heartbeat') {
       console.log('received: %s', message)
     }
-    if (message.startsWith(`Player:`)) {
+    if (message.match(/^Player:[0-9], Move: .*, Position: .*/)) {
+      playersInGame[message.match(/^Player:([0-9]), Move: .*, Position: .*/)[0]].position = message.match(/^Player:[0-9], Move: .*, Position: (.*)/)[0]
+    }
+    if (message.startsWith('Player:')) {
       wss.clients.forEach(function each (client) {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(message)
@@ -33,7 +37,6 @@ wss.on('connection', function connection (ws) {
       })
     }
   })
-  
   ws.on('close', () => {
     if (!wss.clients.size) {
       players = [1]
