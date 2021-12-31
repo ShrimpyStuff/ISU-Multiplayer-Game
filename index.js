4349d5d736eb104790ed0cc68610cac3a83194e0
ShrimpyStuff <sajidmonowar11@gmail.com>
2021-12-30 23:18:40 -0500

    YAY!!!! LOGIN WORKS. I KNOW THAT NOT SENDING THE TOKEN WOULD LET YOU JOIN THE GAME

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const http = require('http').createServer(app)
 const WebSocket = require('ws')
 const bodyParser = require('body-parser')
+const mysql = require('mysql')
+require('dotenv').config()
 
 app.get('/g&p', function (req, res) {
   res.sendFile(__dirname + '/ground and platforms')
@@ -10,8 +12,43 @@ app.get('/g&p', function (req, res) {
 
 app.use(bodyParser.json())
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/login', function (req, res) {
   res.sendFile(__dirname + '/index.html')
+})
+
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB
+})
+
+let recentlyUsedTokens = []
+
+function newToken() {
+  let temp = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+  if (!recentlyUsedTokens.includes(temp)) {return temp} else {temp = newToken()}
+}
+
+app.post('/login', function (req, res) {
+  let username = req.body.username
+  let password = req.body.password
+  pool.query(`SELECT \`password\` FROM \`logins\` WHERE \`username\` = (SELECT \`username\` FROM logins WHERE \`username\` = '${username}')`, (err, result, fields) => {
+    if (err) {
+      res.redirect('/login')
+      throw err
+    }
+    if (password === result[0].password) {
+      let randomToken = newToken()
+      let object = {token: randomToken, username}
+      recentlyUsedTokens.push(object)
+      res.send(randomToken)
+    } else {
+      return res.redirect('/login')
+    }
+  })
 })
 
 const wss = new WebSocket.Server({ server: http })
@@ -20,19 +57,19 @@ let players = [1]
 let playersInGame = [];
 
 wss.on('connection', function connection (ws) {
-  ws.send('SendLoginDetails')
-
-  app.post('/login', function (req, res) {
-    if (new RegExp('Username: .*, Password: .*').test(req.body)) {
-      let username = req.body.match(/Username: (.*),/)
-      let password = req.body.match(/Password: (.*)/)
-      if (username && password) {
-      } else {
-        ws.close()
+  let username = '';
+  ws.on('message', (message) => {
+    if (message.startsWith('Token:')) {
+      for (i=0; i<recentlyUsedTokens.length; i++){
+        if (message.split(':')[1] === recentlyUsedTokens[i].token) {
+          username = recentlyUsedTokens[i].username
+          break
+        } else {
+          ws.close()
+        }
       }
     }
   })
-
   const number = players[players.length - 1]
   if (wssMain.clients.size > 1) {
     ws.send(`Players-In-Game: ${JSON.stringify(playersInGame)}`);
@@ -40,6 +77,10 @@ wss.on('connection', function connection (ws) {
   playersInGame.push({});
   playersInGame[number-1].name = number.toString();
   playersInGame[number-1].position = "0, 0, 0";
+  
+  wss.clients.forEach(function each(client) {
+    client.send(`Player:${number}, Portal:${pool.query(`SELECT \`PortalNumber\` FROM \`logins\` WHERE \`username\` = '${username}')`)}`)
+  })
 
   wss.clients.forEach(function each(client) {
     if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -49,6 +90,14 @@ wss.on('connection', function connection (ws) {
   players.push((number + 1))
 
   ws.on('message', (message) => {
+    if (message.startsWith('Portal:')) {
+      wss.clients.forEach(function each (client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(`Player:${number}, ${message}`)
+        }
+      })
+      pool.query(`UPDATE \`logins\` SET \`PortalNumber\` = \`${message.split(':')[1]}\` WHERE username=\`${username}\``, )
+    }
     if (message.startsWith('Move:')) {
       wss.clients.forEach(function each (client) {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -81,4 +130,6 @@ wss.on('connection', function connection (ws) {
   })
 })
 
-http.listen(process.env.PORT || 8080)
+http.listen((process.env.PORT || 8080), () => {
+  console.log('Started')
+})
